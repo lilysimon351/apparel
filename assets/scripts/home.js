0ad92072cc620782f04cfe00af559cb4439f9e96
Shushanik Simonyan <43413082+lilysimon351@users.noreply.github.com>
2022-10-17 17:26:06 +0400

    hero, choise, about, adaptivity, favicon

---
+++ assets/scripts/home.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    centeredSlides: true,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.choise__pagination',
+      clickable: true
+    },
+
+  });
