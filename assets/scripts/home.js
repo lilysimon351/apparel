a95424d0052c61fdd819e4bfca4fab56f4a055d4
Shushanik Simonyan <43413082+lilysimon351@users.noreply.github.com>
2022-10-18 20:23:45 +0400

    catalog, producer, usage adaptivity; slider corrections

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,12 +1,76 @@
-const swiper = new Swiper('.swiper', {
+new Swiper('.choise__slider', {
     // Optional parameters
     centeredSlides: true,
+    spaceBetween: 40,
     loop: true,
-  
     // If we need pagination
     pagination: {
       el: '.choise__pagination',
       clickable: true
     },
 
-  });
+});
+
+let catalogSwiper;
+let init = false;
+function swiperMode() {
+  let mobile = window.matchMedia("(max-width: 767px)");
+    console.log("DFS")
+  if (mobile.matches) {
+    if (!init) {
+      init = true;
+      catalogSwiper = new Swiper('.catalog__slider', {
+        // Optional parameters
+        centeredSlides: true,
+        loop: true,
+        spaceBetween: 40,
+        
+        // If we need pagination
+        pagination: {
+          el: '.catalog__pagination',
+          clickable: true
+        },
+    
+    });
+    }
+  } else {
+    catalogSwiper?.destroy();
+    init = false;
+  }
+}
+
+window.addEventListener("load", function () {
+  swiperMode();
+});
+
+window.addEventListener("resize", function () {
+  swiperMode();
+});
+
+
+new Swiper('.producer__slider', {
+    // Optional parameters
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    // slidesPerView: 3,
+    initialSlide: 1,
+    spaceBetween: 0,
+    setWrapperSize: true, 
+    // Responsive breakpoints
+    breakpoints: {
+        768: {
+            spaceBetween: 0,
+        },
+        300: {
+            spaceBetween: 0,
+            slidesPerView: 1
+        },
+    },
+    // If we need pagination
+    pagination: {
+      el: '.producer__pagination',
+      clickable: true
+    },
+
+});
